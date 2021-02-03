e1e7c2fb05b6240d94be250198208f3f3933e851
Breno Cocheto <48560880+bcocheto@users.noreply.github.com>
2021-02-03 20:54:44 -0300

    feat: Criado módulo de clientes e controlador
    Criado módulo de clientes e controlador com as funções de listar e adicionar clientes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Cliente;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,27 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $clientes = Cliente::all();
+        return view('home', ['clientes'=>$clientes]);
     }
+
+    public function criar(Request $request){
+
+    if ($request->isMethod('get')) {
+    
+        return view('cliente.criar');
+    
+        }
+    if ($request->isMethod('post')) {
+    $nome = $request->input('nome');
+    $razao_social = $request->input('razao_social');
+    $cnpj = $request->input('cnpj'); 
+    $data_inclusao = date('d-m-Y H:i:s');
+    $data = array( "nome" => $nome, "razao_social" => $razao_social, "cnpj" => $cnpj, "data_inclusao" => $data_inclusao);
+    Cliente::create($data);
+    return redirect()->route('home');
+
+        }
+    }
+
 }
